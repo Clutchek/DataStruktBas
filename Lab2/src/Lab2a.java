b589a7132c9db53f998669c107875e31e4f6ac7d
Rasmus <rasmus.webs@gmail.com>
2016-02-01 13:56:24 +0100

    commit

--- Lab2/src/Lab2a.java
+++ Lab2/src/Lab2a.java
@@ -1,7 +1,23 @@
 public class Lab2a {
   public static double[] simplifyShape(double[] poly, int k)
   {
+      double l1,l2,l3;
 
 
+
+      while(poly.length > k){
+        for(int i = 2; i<poly.length; i++){
+
+        }
+      }
+
+
+    return new double[];
+
+  }
+
+  private double getDistance(int x1,int y1,int x2,int y2){
+
+    return Math.sqrt((Math.pow(x1-x2,2))+(Math.pow(y1-y2,2)));
   }
 }
