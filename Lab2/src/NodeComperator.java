bacaf82864b4ba0b16c3a603995c687b8a7ccb42
Rasmus <rasmus.webs@gmail.com>
2016-02-02 12:43:12 +0100

    comperator

---
+++ Lab2/src/NodeComperator.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+
+/**
+ * Created by Rasmus on 2016-02-02.
+ */
+public class NodeComperator implements Comparator<DLList.Node>{
+
+    @Override
+    public int compare(DLList.Node a, DLList.Node b) {
+
+        if ((double)a.elt < (double)b.elt) return -1;
+        if ((double)a.elt > (double)b.elt) return 1;
+        return 0;
+    }
+}
