9f6e6926931923da30a8cb3f8eb95de053c204bf
Rasmus <rasmus.webs@gmail.com>
2016-02-02 14:41:07 +0100

    Point

---
+++ Lab2/src/Point.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Rasmus on 2016-02-02.
+ */
+public class Point implements Comparable<Point>{
+
+    private double x,y;
+
+    private double value;
+
+    public Point(int x,int y,int value){
+        this.x = x;
+        this.y = y;
+        this.value = value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if (this.value < o.value) return -1;
+        if (this.value > o.value) return 1;
+        return 0;
+    }
+}
