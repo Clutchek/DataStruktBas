0a26c142bd42ab07c346314fa00d8ad858b1e534
Rasmus <rasmus.webs@gmail.com>
2016-02-12 14:06:23 +0100

    SplayTreeSet

---
+++ TDA416/src/Lab3/SplayTreeSet.java
@@ -0,0 +1,26 @@
+package Lab3;
+
+/**
+ * Created by Rasmus on 2016-02-12.
+ */
+public class SplayTreeSet<E> implements SimpleSet {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean add(Comparable x) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Comparable x) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Comparable x) {
+        return false;
+    }
+}
