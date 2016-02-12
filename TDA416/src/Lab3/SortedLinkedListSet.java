fc035ce40c440a07f0d8b44f385608ef5d26cf22
Rasmus <rasmus.webs@gmail.com>
2016-02-12 14:04:48 +0100

    SortedLinkedListSet

--- TDA416/src/Lab3/SortedLinkedListSet.java
+++ TDA416/src/Lab3/SortedLinkedListSet.java
@@ -3,8 +3,26 @@ package Lab3;
 /**
  * Created by Rasmus on 2016-02-12.
  */
-public class SortedLinkedListSet{
+public class SortedLinkedListSet<E> implements SimpleSet{
 
 
+    @Override
+    public int size() {
+        return 0;
+    }
 
+    @Override
+    public boolean add(Comparable x) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Comparable x) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Comparable x) {
+        return false;
+    }
 }
