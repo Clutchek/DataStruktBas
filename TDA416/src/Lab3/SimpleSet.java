e43109e57cddfb00363224cd229f55cafeb4a2dd
Rasmus <rasmus.webs@gmail.com>
2016-02-12 14:00:49 +0100

    temp

---
+++ TDA416/src/Lab3/SimpleSet.java
@@ -0,0 +1,8 @@
+package Lab3;
+
+interface SimpleSet<E extends Comparable<? super E>> {
+ int size();
+ boolean add(E x);
+ boolean remove(E x);
+ boolean contains(E x);
+}
---
+++ TDA416/src/Lab3/SortedLinkedListSet.java
@@ -0,0 +1,10 @@
+package Lab3;
+
+/**
+ * Created by Rasmus on 2016-02-12.
+ */
+public class SortedLinkedListSet{
+
+
+
+}
