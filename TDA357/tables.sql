f59fe0763272db44e639cb7b2d281ff278054f10
Rasmus <rasmus.webs@gmail.com>
2016-02-16 11:14:10 +0100

    pre,ap,res

--- TDA357/tables.sql
+++ TDA357/tables.sql
@@ -16,3 +16,18 @@ CREATE TABLE student(
 	name VARCHAR NOT NULL, 
 	studyProgramme VARCHAR NOT NULL, 
 	branch VARCHAR default NULL);
+	
+
+CREATE TABLE Prerequisites(
+	course VARCHAR NOT NULL references course(courseCode),
+	requredCourse VARCHAR NOT NULL references course(courseCode),
+	PRIMARY KEY (course, requredCourse));
+
+CREATE TABLE AppliedFor(
+	s
+	
+CREATE TABLE IsAttending(
+	student INT NOT NULL references student(personalCodeNumber),
+	course VARCHAR NOT NULL references course(courseCode),
+	PRIMARY KEY (student,course));
+
