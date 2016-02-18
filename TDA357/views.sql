79c1c690d4b1af2f1a73e31767f0c5d5c6ee8cb7
Rasmus <rasmus.webs@gmail.com>
2016-02-18 13:45:17 +0100

    VIEW PassedCourses

--- TDA357/views.sql
+++ TDA357/views.sql
@@ -7,10 +7,16 @@ CREATE VIEW FinishedCourses AS
 	SELECT student, coursecode, name, grade ,credits
 	FROM graded,course WHERE graded.course = course.courseCode;
 
-	CREATE VIEW Registrations AS
+CREATE VIEW Registrations AS
 	SELECT restrictedCourse AS course, student, 'waiting' AS Status
 	FROM appliedFor
 	UNION
 	SELECT course, student, 'registered' AS Status
 	FROM isAttending)
 	ORDER BY course;
+
+CREATE VIEW PassedCourses AS
+	SELECT student, coursecode, name, grade ,credits
+	FROM graded,course WHERE graded.course = course.courseCode AND graded.grade != 'U';
+
+
