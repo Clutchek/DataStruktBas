fb228f0206551d675a101c979199940086dd5e5d
Alexander Kloutschek <nalex.k@hotmail.com>
2016-02-19 08:43:24 +0100

    Fixed classification points

--- TDA357/views.sql
+++ TDA357/views.sql
@@ -35,11 +35,48 @@ CREATE VIEW UnreadMandatory AS
 	WHERE (student, course)  NOT IN (SELECT student, coursecode from PassedCourses);
 
 CREATE VIEW PathToGraduation AS
- SELECT student.personalcodenumber, sum(passedcourses.credits) AS sum
-   FROM student
-   LEFT JOIN passedcourses ON student.personalcodenumber = passedcourses.student
-  GROUP BY student.personalcodenumber
-  ORDER BY student.personalcodenumber, sum;
+ 	SELECT student.personalcodenumber, sum(passedcourses.credits) AS sum
+	FROM student
+	LEFT JOIN passedcourses ON student.personalcodenumber = passedcourses.student
+	GROUP BY student.personalcodenumber
+	ORDER BY student.personalcodenumber, sum;
+
+  CREATE VIEW PathToGraduation2 AS
+	SELECT student.personalcodenumber as student,
+ 	sum( CASE WHEN classification = 'Math' THEN PassedClass.credits ELSE 0 END) as mathCredits,
+ 	sum( CASE WHEN classification = 'Research' THEN PassedClass.credits ELSE 0 END) as researchCredits,
+ 	sum( CASE WHEN classification = 'Seminar' THEN 1 ELSE 0 END) as nbrOfSeminarCourses
+   	FROM student LEFT JOIN PassedClass ON student.personalcodenumber = PassedClass.student
+  	GROUP BY student.personalcodenumber
+  	ORDER BY student.personalcodenumber;
+
+  	CREATE VIEW PassedClass AS
+  		SELECT * from (passedcourses JOIN courseClassification on passedcourses.coursecode = courseclassification.course);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--backup
+
+  CREATE VIEW PathToGraduation2 AS
+	SELECT student.personalcodenumber as student, sum(passedcourses.credits) AS credits,
+ 	sum( CASE WHEN classification = 'Math' THEN passedcourses.credits ELSE 0 END) as mathCredits
+   	FROM (student LEFT JOIN (SELECT * from (passedcourses JOIN courseClassification on passedcourses.coursecode = courseclassification.course) as passedClass ON student.personalcodenumber = passedClass.student)) as fromTable
+  	GROUP BY student.personalcodenumber
+  	ORDER BY student.personalcodenumber, sum;
