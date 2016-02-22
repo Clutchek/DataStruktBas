65e13af24ff3c685822d5dcc0e6b3f995f1b0cd8
rasmusgit <rasmus.webs@gmail.com>
2016-02-22 11:12:07 +0100

    SplayTreeSet

--- TDA416/src/Lab3/SplayTreeSet.java
+++ TDA416/src/Lab3/SplayTreeSet.java
@@ -4,9 +4,36 @@ package Lab3;
  * Created by Rasmus on 2016-02-12.
  */
 public class SplayTreeSet <E extends Comparable<? super E>>  implements SimpleSet<E> {
+
+    public class Node {
+        /** The contents of the node is public */
+        public E elt;
+
+        protected Node prev, next;
+
+        Node() {
+            this(null);
+        }
+        Node(E elt) {
+            this.elt = elt;
+            prev = next = null;
+        }
+
+
+        public Node getNext() {
+            return this.next;
+        }
+
+        public Node getPrev() {
+            return this.prev;
+        }
+    }
+
+    private int size;
+
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
