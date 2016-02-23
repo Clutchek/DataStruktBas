b5fb24b989c254ba2e223ae45118b15598abdbe7
Alexander Kloutschek <nalex.k@hotmail.com>
2016-02-23 10:33:01 +0100

    Fixed right and left rotate

--- TDA416/src/Lab3/SplayTreeSet.java
+++ TDA416/src/Lab3/SplayTreeSet.java
@@ -9,14 +9,14 @@ public class SplayTreeSet <E extends Comparable<? super E>>  implements SimpleSe
         /** The contents of the node is public */
         public E elt;
 
-        protected Node prev, next;
+        protected Node parent, left, right;
 
         Node() {
             this(null);
         }
         Node(E elt) {
             this.elt = elt;
-            prev = next = null;
+            parent = left = right = null;
         }
 
 
@@ -30,6 +30,7 @@ public class SplayTreeSet <E extends Comparable<? super E>>  implements SimpleSe
     }
 
     private int size;
+    private Node root;
 
     @Override
     public int size() {
@@ -51,5 +52,50 @@ public class SplayTreeSet <E extends Comparable<? super E>>  implements SimpleSe
         return false;
     }
 
+    public void rotateRight(Node node){
+        Node left = node.left;
+        if(left != null) {
+            node.left = right.right;
+            if (right.right != null) {
+                left.right.parent = node;
+            }
+            left.parent = node.parent;
+        }
+        if(node.parent == null){
+            root = left;
+        }else if(node == node.parent.right){
+            node.parent.right = left;
+        }else{
+            node.parent.left = left;
+        }
+        if(left != null){
+            left.right = node;
+        }
+        node.parent = left;
+
+    }
+
+    public Node rotateLeft(Node node){
+        Node right = node.right;
+        if(right != null) {
+            node.right = right.left;
+            if (right.left != null) {
+                right.left.parent = node;
+            }
+            right.parent = node.parent;
+        }
+        if(node.parent == null){
+            root = right;
+        }else if(node == node.parent.right){
+            node.parent.right = right;
+        }else{
+            node.parent.left = right;
+        }
+        if(right != null){
+            right.left = node;
+        }
+        node.parent = right;
+
+    }
 
 }
