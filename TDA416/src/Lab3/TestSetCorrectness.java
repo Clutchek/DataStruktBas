64d91148feccb89507b1ea9d233747f756319f7f
Alexander Kloutschek <nalex.k@hotmail.com>
2016-02-24 14:52:43 +0100

    Renamed tsc to tsc2 and added a new test class

--- TDA416/src/Lab3/TestSetCorrectness.java
+++ TDA416/src/Lab3/TestSetCorrectness.java
@@ -1,57 +1,15 @@
 package Lab3;
 
-import java.util.Random;
-import java.util.TreeSet;
-
-
+/**
+ * Created by clutchek on 2/24/16.
+ */
 public class TestSetCorrectness {
-    public static void main(String[] args) {
-        int n1 = Integer.parseInt(args[0]);
+
+    public static void main(String[] args){
+        int test  = Integer.parseInt(args[1]);
         int n2 = Integer.parseInt(args[1]);
         int n3 = Integer.parseInt(args[2]);
         int n4 = Integer.parseInt(args[3]);
 
-        if(n1 == 1){
-            //TEST
-        }else if(n1 == 2){
-            testSplayTree(n2,n3,n4);
-        }
-    }
-
-    private static void testSplayTree(int n2, int n3, int n4){
-        Random randomGen = new Random();
-        for(int i = 0; i < n2; i++){
-            SplayTreeSet<Integer> splayTree = new SplayTreeSet<>();
-            TreeSet<Integer> treeSet = new TreeSet<>();
-            for(int j = 0; j < n3; j++){
-                double random = Math.random();
-                if(random < 0.25){
-                    int randInt = randomGen.nextInt(n4);
-                    if(!(splayTree.add(randInt) == treeSet.add(randInt))){
-                        System.out.println("Error on add");
-                        System.exit(1);
-                    }
-
-                }else if(random < 0.5){
-                    int randInt = randomGen.nextInt(n4);
-                    if(!(splayTree.remove(randInt) == treeSet.remove(randInt))){
-                        System.out.println("Error on remove");
-                        System.exit(2);
-                    }
-
-                }else if(random < 0.75){
-                    int randInt = randomGen.nextInt(n4);
-                    if(!(splayTree.contains(randInt) == treeSet.contains(randInt))){
-                        System.out.println("Error on contains");
-                        System.exit(3);
-                    }
-                }else{
-                    if(!(splayTree.size() == treeSet.size())){
-                        System.out.println("Error on size");
-                        System.exit(4);
-                    }
-                }
-            }
-        }
     }
 }
---
+++ TDA416/src/Lab3/TestSetCorrectness2.java
@@ -0,0 +1,94 @@
+package Lab3;
+
+import java.util.Random;
+import java.util.TreeSet;
+
+
+public class TestSetCorrectness2 {
+    public static void main(String[] args) {
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[1]);
+        int n3 = Integer.parseInt(args[2]);
+        int n4 = Integer.parseInt(args[3]);
+
+        if(n1 == 1){
+            testLinkedList(n2,n3,n4);
+        }else if(n1 == 2){
+            testSplayTree(n2,n3,n4);
+        }
+    }
+
+    private static void testSplayTree(int n2, int n3, int n4){
+        Random randomGen = new Random();
+        for(int i = 0; i < n2; i++){
+            SplayTreeSet<Integer> splayTree = new SplayTreeSet<>();
+            TreeSet<Integer> treeSet = new TreeSet<>();
+            for(int j = 0; j < n3; j++){
+                double random = Math.random();
+                if(random < 0.25){
+                    int randInt = randomGen.nextInt(n4);
+                    if(!(splayTree.add(randInt) == treeSet.add(randInt))){
+                        System.out.println("Error on add");
+                        System.exit(1);
+                    }
+
+                }else if(random < 0.5){
+                    int randInt = randomGen.nextInt(n4);
+                    if(!(splayTree.remove(randInt) == treeSet.remove(randInt))){
+                        System.out.println("Error on remove");
+                        System.exit(2);
+                    }
+
+                }else if(random < 0.75){
+                    int randInt = randomGen.nextInt(n4);
+                    if(!(splayTree.contains(randInt) == treeSet.contains(randInt))){
+                        System.out.println("Error on contains");
+                        System.exit(3);
+                    }
+                }else{
+                    if(!(splayTree.size() == treeSet.size())){
+                        System.out.println("Error on size");
+                        System.exit(4);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void testLinkedList(int n2, int n3, int n4){
+        Random randomGen = new Random();
+        for(int i = 0; i < n2; i++){
+            SortedLinkedListSet<Integer> splayTree = new SortedLinkedListSet<>();
+            TreeSet<Integer> treeSet = new TreeSet<>();
+            for(int j = 0; j < n3; j++){
+                double random = Math.random();
+                if(random < 0.25){
+                    int randInt = randomGen.nextInt(n4);
+                    if(!(splayTree.add(randInt) == treeSet.add(randInt))){
+                        System.out.println("Error on add");
+                        System.exit(1);
+                    }
+
+                }else if(random < 0.5){
+                    int randInt = randomGen.nextInt(n4);
+                    if(!(splayTree.remove(randInt) == treeSet.remove(randInt))){
+                        System.out.println("Error on remove");
+                        System.exit(2);
+                    }
+
+                }else if(random < 0.75){
+                    int randInt = randomGen.nextInt(n4);
+                    if(!(splayTree.contains(randInt) == treeSet.contains(randInt))){
+                        System.out.println("Error on contains");
+                        System.exit(3);
+                    }
+                }else{
+                    if(!(splayTree.size() == treeSet.size())){
+                        System.out.println("Error on size");
+                        System.exit(4);
+                    }
+                }
+            }
+        }
+    }
+}
