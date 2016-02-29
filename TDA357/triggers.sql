9204a2682de1a1dc69521593d1781684432499ed
Alexander Kloutschek <nalex.k@hotmail.com>
2016-02-29 10:20:09 +0100

    Added more if cases to first trigger

--- TDA357/triggers.sql
+++ TDA357/triggers.sql
@@ -1,6 +1,33 @@
 CREATE FUNCTION regCheck() RETURNS trigger AS $$
     BEGIN
 
+        --null, kurskod existerar
+        IF NEW.student IS NULL OR NEW.course IS NULL THEN
+            RAISE EXCEPTION 'Input was null'
+
+        IF NOT EXISTS(SELECT course FROM course WHERE course.coursecode = NEW.course) THEN
+            RAISE EXCEPTION 'Chosen course does not exist'
+
+        IF NOT EXISTS(SELECT personalCodeNumber FROM student WHERE student.personalCodeNumber = NEW.student) THEN
+            RAISE EXCEPTION 'Student does not exist'
+
+        IF EXISTS(SELECT student FROM isAttending WHERE isAttending.student = New.student AND isAttending.course = NEW.course)
+        OR EXISTS(SELECT student FROM PassedCourses WHERE PassedCourses.student = New.student AND PassedCourses.coursecode = NEW.course)
+        THEN
+            RAISE EXCEPTION '% is already attending or have passed this course', NEW.student;
+        END IF;
+
+        nbrOfRequiredCourses := (SELECT COUNT(course) FROM prerequisites WHERE course = NEW.course);
+
+        nbrOfPassedCourses := (SELECT COUNT(student) FROM prerequisites JOIN PassedCourses on requiredCourse = coursecode WHERE 
+            AND  student = NEW.student 
+            AND course = NEW.course);
+
+        IF (nbrOfRequiredCourses > nbrOfPassedCourses)
+        THEN
+            RAISE EXCEPTION '% have not passed all required courses', NEW.student;
+        END IF;
+
         nbrOfRegisterend := (SELECT Count(student)
                             FROM Registrations
                             WHERE course = NEW.course);
@@ -10,21 +37,11 @@ CREATE FUNCTION regCheck() RETURNS trigger AS $$
                             WHERE course = NEW.course);
 
         IF nbrOfRegisterend >= maxNbrOfStudents THEN
-            INSERT values() INTO appliedFor values(NEW.student, NEW.course, default);
+            INSERT values(NEW.student, NEW.course, default) INTO appliedFor;
+        ELSE
+            INSERT values(NEW.student, NEW.course);
         END IF;
 
-        IF EXISTS(SELECT student FROM isAttending WHERE isAttending.student = New.student AND isAttending.course = NEW.course);
-        OR EXISTS(SELECT student FROM PassedCourses WHERE PassedCourses.student = New.student AND PassedCourses.coursecode = NEW.course);
-        THEN
-            RAISE EXCEPTION '% is already attending or have passed this course', NEW.student;
-        END IF;
-
-        IF (NOT EXISTS(SELECT student FROM prerequisites JOIN PassedCourses on requiredCourse = coursecode WHERE 
-            AND  student = NEW.student 
-            AND course = NEW.course))
-        THEN
-            RAISE EXCEPTION '% have not passed all required courses', NEW.student;
-        END IF;
 
         RETURN NEW;
     END;
